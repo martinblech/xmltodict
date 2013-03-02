9ca88cedfb74d1543ec4c70c6ca434e756452c6f
Martin Blech <martinblech@gmail.com>
2013-03-02 13:19:39 -0300

    bump version number to 0.4.5

--- xmltodict.py
+++ xmltodict.py
@@ -26,7 +26,7 @@ except NameError: # pragma no cover
     _unicode = str
 
 __author__ = 'Martin Blech'
-__version__ = '0.4.4'
+__version__ = '0.4.5'
 __license__ = 'MIT'
 
 class ParsingInterrupted(Exception): pass
