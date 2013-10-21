4aa53562f0bc3efc22ed6aeb3af3b68737c78ec1
Martin Blech <martinblech@gmail.com>
2013-10-21 10:55:11 +0200

    Use system setuptools if available.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-from ez_setup import use_setuptools
-use_setuptools()
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
-from setuptools import setup
 import xmltodict
 
 setup(name='xmltodict',
