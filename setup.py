martinblech/xmltodict

commit 8374e7d84f9209808373d26a7bbeed3e567ed892

Use Markdown long_description on PyPI (#190)

setup.py
@@ -8,9 +8,15 @@ except ImportError:
 
 import xmltodict
 
+with open('README.md') as f:
+    long_description = f.read()
+
+
 setup(name='xmltodict',
       version=xmltodict.__version__,
       description=xmltodict.__doc__,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author=xmltodict.__author__,
       author_email='martinblech@gmail.com',
       url='https://github.com/martinblech/xmltodict',

commit a2325c96ba2ec4af4472c7398edf65c94634f96e

setup: open README.md with a specific encoding
With python3, the built-in open() function opens files in "text" mode by
default (i.e. the f.read() method reads bytes and tries to decode them
to unicode using the interpreter default encoding). Depending on the
system locale, this default encoding may be 'ascii'. Which leads to the
following error:

 ~$ python3 setup.py egg_info
 Traceback (most recent call last):
   File "setup.py", line 12, in <module>
     long_description = f.read()
   File "/usr/lib/python3.6/encodings/ascii.py", line 26, in decode
     return codecs.ascii_decode(input, self.errors)[0]
 UnicodeDecodeError: 'ascii' codec can't decode byte 0xc3 in position
 2587: ordinal not in range(128)

The character at position 2587 is 'ô' (from Fantômas) and is utf-8
encoded.

Force to open the file in "binary" mode (which is the only mode on
python2) and explicitly decode the read bytes to unicode using the
'utf-8' codec.

Fixes: 8374e7d84f92 ("Use Markdown long_description on PyPI (#190)")
Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

setup.py
@@ -8,8 +8,8 @@ except ImportError:
 
 import xmltodict
 
-with open('README.md') as f:
-    long_description = f.read()
+with open('README.md', 'rb') as f:
+    long_description = f.read().decode('utf-8')
 
 
 setup(name='xmltodict',