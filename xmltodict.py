8d7899b40852778b631c9ec90d007bed44cecf85
Martin Blech <78768+martinblech@users.noreply.github.com>
2026-08-11 01:07:30 -0700

    chore(unparse): remove unused escape import

--- xmltodict.py
+++ xmltodict.py
@@ -2,7 +2,7 @@
 "Makes working with XML feel like you are working with JSON"
 
 from xml.parsers import expat
-from xml.sax.saxutils import XMLGenerator, escape
+from xml.sax.saxutils import XMLGenerator
 from xml.sax.xmlreader import AttributesImpl
 from io import StringIO
 from inspect import isgenerator
